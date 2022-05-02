c2d4457e756065471532e1907981e4d5d01c06a1
sominparrrk <itsmechloe@naver.com>
2022-05-03 01:11:18 +0900

    Chore: Set up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'blue-base': '#156aff',
+        'red-base': '#e52f2f',
+        'green-base': '#08b468',
+        'purple-base': '#6400ff',
+      },
+      screens: {
+        '3xl': '1600px',
+      },
+    },
+  },
+  plugins: [],
+};
